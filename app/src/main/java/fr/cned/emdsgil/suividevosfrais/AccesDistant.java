a6a4548708ee7b0c693820d5bd3ec98ed3a7427d
Gdlsd <38458974+Gdlsd@users.noreply.github.com>
2019-02-24 18:37:16 +0100

    Update AccesDistant.java

--- app/src/main/java/fr/cned/emdsgil/suividevosfrais/AccesDistant.java
+++ app/src/main/java/fr/cned/emdsgil/suividevosfrais/AccesDistant.java
@@ -14,7 +14,7 @@ import fr.cned.emdsgil.suividevosfrais.outils.AsyncResponse;
 public class AccesDistant implements AsyncResponse {
 
     //constante
-    private static final String SERVERADDR = "http://192.168.1.18/suividevosfrais/serveursuivifrais.php";
+    private static final String SERVERADDR = "http://*******/suividevosfrais/serveursuivifrais.php";
     private Controle controle;
 
 
